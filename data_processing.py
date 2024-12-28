00fad0bdd0e2592da52aef7bd27480ce6f26416f
TaeHo Kim <thkim210414@gmail.com>
2024-12-28 16:02:41 -0500

    Create/update data_processing.py

---
+++ data_processing.py
@@ -0,0 +1,9 @@
+import pandas as pd
+from config import DATA_PATH, DATE_COL
+
+def load_data():
+    df = pd.read_csv(DATA_PATH)
+    df[DATE_COL] = pd.to_datetime(df[DATE_COL], dayfirst=True, utc=True, infer_datetime_format=True)
+    df.sort_values(by=DATE_COL, inplace=True)
+    df.reset_index(drop=True, inplace=True)
+    return df
